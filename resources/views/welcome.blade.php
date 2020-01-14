fcbe50c58f09837909fb4a837a80329014c8cfac
LNdame <ls20045@gmail.com>
2020-01-14 16:39:13 +0200

    quick title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    QUICK EATS
                 </div>
 
                 <div class="links">
